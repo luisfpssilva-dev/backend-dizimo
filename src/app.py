d8f2f5bf95e699e65d9c38f29ef408c74234c8ea
Marcos Henrique <marcoshenrique@Marcoss-MacBook-Pro.local>
2024-07-04 09:06:43 -0300

    Add CORS

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 import sys
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -12,7 +13,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'lmi')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///dizimo.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-
+CORS(app)
 from src.db import db
 db.init_app(app)
 
